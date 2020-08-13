9fffe284e76e0436cb7e71412dac6fad0c5508fa
Sanjaya <vimuckthie@gmail.com>
2020-08-13 17:31:55 +0300

    init

--- js/main.js
+++ js/main.js
@@ -13,20 +13,12 @@ $(".nav a").on("click", function () {
 $(document).ready(function () {
   $(window).on("scroll", function () {
     if ($(window).scrollTop() < 1000) {
-      $("hero").css(
+      $(".hero").css(
         "background-size",
         130 + parseInt($(window).scrollTop() / 5) + "%"
       );
       $(".herotext").css("top", 50 + $(window).scrollTop() * 0.1 + "%");
       $(".herotext").css("opacity", 1 - $(window).scrollTop() * 0.003);
-    }
-
-    if ($(window).scrollTop() >= $(".content-wrapper").offset().top - 300) {
-      $(".nav-bg").removeClass("bg-hidden");
-      $(".nav-bg").addClass("bg-visible");
-    } else {
-      $(".nav-bg").removeClass("bg-visible");
-      $(".nav-bg").addClass("bg-hidden");
     }
   });
 });
