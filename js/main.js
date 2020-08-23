5e56163feca771ab07f9ab31194bc82cc71ae9e0
Sanjaya <vimuckthie@gmail.com>
2020-08-23 14:03:46 +0300

    init

--- js/main.js
+++ js/main.js
@@ -31,4 +31,13 @@ $(document).ready(function () {
   });
 });
 
-/*** SVG ***/
+/*** slider ***/
+
+$(document).ready(function () {
+  $("#owl-demo").owlCarousel({
+    navigation: true, // Show next and prev buttons
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    singleItem: true,
+  });
+});
