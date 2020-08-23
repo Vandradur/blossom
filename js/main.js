c4e23affcf5cad35ea8f301d49c86f44ab55cc46
Sanjaya <vimuckthie@gmail.com>
2020-08-23 19:16:16 +0300

    init

--- js/main.js
+++ js/main.js
@@ -32,12 +32,3 @@ $(document).ready(function () {
 });
 
 /*** slider ***/
-
-$(document).ready(function () {
-  $("#owl-demo").owlCarousel({
-    navigation: true, // Show next and prev buttons
-    slideSpeed: 300,
-    paginationSpeed: 400,
-    singleItem: true,
-  });
-});
