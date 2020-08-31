22254499d4d7f6929c57959c7678e6c32dca888b
Sanjaya <vimuckthie@gmail.com>
2020-08-31 22:31:15 +0300

    init

--- js/main.js
+++ js/main.js
@@ -31,4 +31,31 @@ $(document).ready(function () {
   });
 });
 
-/*** slider ***/
+/*** video Gallery ***/
+
+var init = function () {
+  $(".panel").on("mouseover", onPanelMouseIn);
+  $(".panel").on("mouseout", onPanelMouseOut);
+};
+
+var onPanelMouseIn = function (evt) {
+  var $target = $(evt.currentTarget);
+  var isPlaying = $target.hasClass("active");
+  if (isPlaying) return;
+  var video = $target.find("video")[0];
+  video.play();
+  $target.addClass("active");
+};
+
+var onPanelMouseOut = function (evt) {
+  var $target = $(evt.currentTarget);
+  var isPlaying = $target.hasClass("active");
+  if (!isPlaying) return;
+  var video = $target.find("video")[0];
+  video.pause();
+  $target.removeClass("active");
+};
+
+$(window).ready(init);
+
+/*--- 2 Video ---*/
