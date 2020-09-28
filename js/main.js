f704f7abd56929a8cec997504c731c84d349bac9
Sanjaya <vimuckthie@gmail.com>
2020-09-28 18:49:09 +0300

    init

--- js/main.js
+++ js/main.js
@@ -82,4 +82,22 @@ var onPanelMouseOut = function (evt) {
 
 $(window).ready(init);
 
-/*--- slideshow ---*/
+/*--- parallax slide ---*/
+$(function () {
+  $("nav a").on("click", function (e) {
+    console.log(this.hash);
+    if (this.hash !== "") {
+      e.preventDefault();
+
+      const hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800
+      );
+    }
+  });
+  // $('.fade').addClass('active')
+});
